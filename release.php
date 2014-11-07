113c1ef2070a8ed17123b56c133dda35730708dd
GreenyDev <greenydev@greenmail.net.au>
2014-11-07 12:48:41 +1030

    again

--- release.php
+++ release.php
@@ -2,5 +2,5 @@
         include_once 'checker.php';
         
         if (checkUDID($_SERVER['HTTP_X_UNIQUE_ID']) == 'YES')
-		header('Location: http://your_repo.com/folder/release_dir');
+		header('Location: http://greenydev.github.io/Release');
 ?>
