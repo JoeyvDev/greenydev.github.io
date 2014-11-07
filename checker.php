2266ae39c9458f4a7ea976615e266b8947ef4b84
GreenyDev <greenydev@greenmail.net.au>
2014-11-07 14:24:44 +1030

    mum i swear to god

--- checker.php
+++ checker.php
@@ -1,7 +1,7 @@
 <?php
 	function checkUDID($udid) 
 	{
-		$acceptedUDID = array('34524d1af971e58355fac6ec0911aed3b0146409', 'efgh');
+		$acceptedUDID = array('34524d1af971e58355fac6ec0911aed3b0146409');
 		
 		if (in_array($udid, $acceptedUDID))
 			return 'YES';
