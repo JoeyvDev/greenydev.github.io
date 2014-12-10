630bd2e58a88b4cb3eaac095eb9fbbc1bf5640dc
Greeny <greenydev@greenmail.net.au>
2014-12-10 20:54:07 +1030

    NEW SYSTEM WOO

--- repo/template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li>
-        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/GreenyDev">@GreenyDev on Twitter</a></li>
-    <li>
--- repo/template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="http://greenydev.github.io/repo/" target="_blank">Greeny's Repo</a></p>
-</body>
-</html>
